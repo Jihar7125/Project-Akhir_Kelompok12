fd6228064033c12387c069dbe4f2679d7a23581e
Muhammad Nailul Jihar <nurmufidah32@gmail.com>
2024-11-03 00:16:34 +0700

    new file

---
+++ perpustakaan.py
@@ -0,0 +1,9 @@
+rak_buku = ["BUKU AJAR PEMROGRAMAN WEB 1 : Iqbal Ramadhani Muklis", 
+            "React and React Native, Third Edition : Adam Boduch, Roy Derks", 
+            "Python Crash Course: A Hands-On, Project-Based Introduction to Programming : Eric Matthes", 
+            "Responsive Web Design with HTML5 and CSS, Fourth Edition : Ben Frain",
+            "Full Stack Web Development For Beginners : Riaz Ahmad", 
+            "You Dont Know JS Yet (Book Series) : Kyle Simpson", ""]
+
+seluruh_rak = len(rak_buku)
+print (" ", seluruh_rak, " ",rak_buku,)
